a03744f57824d15b331cd277ef88676682f09eb9
Xarly <cargutalo@gmail.com>
2017-08-15 23:00:05 +0200

    update

--- comic_strip_capturer.py
+++ comic_strip_capturer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 
-garfield comic capturer in spanish
-@author: Xarly
+Garfield comic capturer in spanish
+@author: Ylrax
 
 format web: http://www.gocomics.com/garfieldespanol%20/2007/06/01
 #image link example: http://assets.amuniversal.com/39ae74c001cf01358d0a005056a9545d
@@ -17,31 +17,30 @@ import csv
 import urllib.request
 import tqdm
 
-#creation of the dates of the comics strips. Select the dates (short periods only pls)
-check=True
-capt=True
+# Creation of the dates of the comics strips. Select the dates (short periods only pls)
+check = True
+capt = True
 d1 = date(2006, 1, 1)  # start date y m d
 d2 = date(2006, 1, 31)  # end date
-
 delta = d2 - d1         # timedelta
-li=[]
+li = []
 
 for i in tqdm.tqdm(range(delta.days + 1)):
-    temp=d1 + timedelta(days=i)
+    temp = d1 + timedelta(days=i)
     li.append("http://www.gocomics.com/garfieldespanol /"+temp.strftime("%Y/%m/%d"))
     
     
-#check with today comic to avoid invalid links to be resend
+# Check with today comic to avoid invalid links to be resend
 pag = requests.get("http://www.gocomics.com/garfieldespanol")   
 soup = BeautifulSoup(pag.content, 'html.parser')
-one=soup.findAll("picture")
-B=[]
+one = soup.findAll("picture")
+B = []
 
 for x in one:
     B.append(str(x))
 
-p=B[3].split(" ")[4]
-#p[5:-1] contains the taday's img link
+p = B[3].split(" ")[4]
+#p[5:-1] contains the today's img link
     
     
 for i in range(len(li)):
@@ -49,41 +48,38 @@ for i in range(len(li)):
     pag = requests.get(li[i])   
     soup = BeautifulSoup(pag.content, 'html.parser')
 
-    one=soup.findAll("picture")
-    A=[]
+    one = soup.findAll("picture")
+    A = []
 
     for x in one:
         A.append(str(x))
 
-    t=A[3].split(" ")[4]
+    t = A[3].split(" ")[4]
     #t[5:-1] contain the img link
     
     
     #here we get the picture to the folder of the .py file
     try:
-        if t[5:-1]!=p[5:-1]:
-            output="garf_"+li[i][-10:].replace("/", "_")+'.png'
-            urllib.request.urlretrieve(t[5:-1],output)
-            capt=True
-            link="got it"
+        if t[5:-1] != p[5:-1]:
+            output = "garf_"+li[i][-10:].replace("/", "_")+'.png'
+            urllib.request.urlretrieve(t[5:-1], output)
+            capt = True
+            link = "got it"
         else:
-            capt=False
-            link="not in website"
+            capt = False
+            link = "not in website"
             print(link)
-            t=""
+            t = ""
     except:
-        link="fail"
-        capt=False
-        t=""
+        link = "fail"
+        capt = False
+        t = ""
     
     # now paste information in a csv (optional)
-    if pag.status_code!=200:
-        link="error webpage"
-    fields=[li[i][-10:],li[i],t[5:-1],str(capt),link]
+    if pag.status_code != 200:
+        link = "error webpage"
+    fields = [li[i][-10:], li[i], t[5:-1], str(capt), link]
 
-    with open("list_comics.csv", 'a',newline='') as f:
+    with open("list_comics.csv", 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
-
-
-
