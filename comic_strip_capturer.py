b786c7835b588ebfaa23f19e2e3e21db62a7435c
Xarly <cargutalo@gmail.com>
2018-02-04 00:13:47 +0100

    Code optimization

--- comic_strip_capturer.py
+++ comic_strip_capturer.py
@@ -17,55 +17,45 @@ from bs4 import BeautifulSoup
 import csv   
 import urllib.request
 
+print("Starting process")
 # Creation of the dates of the comics strips. Select the dates (short periods only pls, be nice)
 check = True
 capt = True
 year = 2016  # year of the desired comic strip
 
 d1 = date(year, 1, 1)  # start date %y %m %d
-d2 = date(year, 2, 28)  # end date
+d2 = date(year, 1, 1)  # end date
 
 delta = d2 - d1         # timedelta interval for the gathering
-li = []
+
 
 # Creation of the list with the links to get the comic strip
-for i in range(delta.days + 1):    
-    temp = d1 + timedelta(days=i)
-    li.append("http://www.gocomics.com/garfieldespanol /" + temp.strftime("%Y/%m/%d"))
+li = ["http://www.gocomics.com/garfieldespanol /" + (d1 + timedelta(days=i)).strftime("%Y/%m/%d")
+      for i in range(delta.days + 1)]
     
     
 # Check with today comic to avoid invalid links to be resend (inexistent links goes to today's link)
 pag = requests.get("http://www.gocomics.com/garfieldespanol")
 soup = BeautifulSoup(pag.content, 'html.parser')
 one = soup.findAll("picture")
-B = []
 
-for x in one:
-    B.append(str(x))
-
-p = B[1].split(" ")[4]
-# p[5:-1] contains the today's img link
+# contains the today's img link
+today_img = str(one[1]).split(" ")[105][5:-1]
     
 # Loop to extract the pictures
 for i in range(len(li)):
     time.sleep(0.2)
     pag = requests.get(li[i])   
     soup = BeautifulSoup(pag.content, 'html.parser')
-
     one = soup.findAll("picture")
-    A = []
 
-    for x in one:
-        A.append(str(x))
-
-    t = A[1].split(" ")[4]
-    # t[5:-1] contain the img link
+    loop_img = str(one[1]).split(" ")[105][5:-1]
 
     # Here we get the picture to the folder of the .py file
     try:
-        if t[5:-1] != p[5:-1]:
+        if today_img != loop_img:
             output = "garf_" + li[i][-10:].replace("/", "_") + '.png'
-            urllib.request.urlretrieve(t[5:-1], output)
+            urllib.request.urlretrieve(loop_img, output)
             capt = True
             link = "got it"
         else:
@@ -76,13 +66,14 @@ for i in range(len(li)):
     except:
         link = "fail"
         capt = False
-        t = ""
+        loop_img = ""
     
     # Now paste information in a csv as log file list_comics.csv (optional)
     if pag.status_code != 200:
-        link = "error webpage"
-    fields = [li[i][-10:], li[i], t[5:-1], str(capt), link]
+        link = "error web page"
+    fields = [li[i][-10:], li[i], loop_img, str(capt), link]
+    print(fields)
 
-    with open("list_comics.csv", 'a',newline='') as f:
+    with open("list_comics.csv", 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
